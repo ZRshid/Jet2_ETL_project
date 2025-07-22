68141e90add2de7efc6e833fd2951406aeb67d4e
Zakii <zakiii1995@outlook.com>
2025-07-22 16:22:59 +0100

    create s3 buckets to store both raw and processed data using TF

--- Terraform/s3.tf
+++ Terraform/s3.tf
@@ -1,6 +1,6 @@
 
 resource "aws_s3_bucket" "raw_data_bucket" {
-  bucket = var.raw_data_bucket
+  bucket_prefix = var.raw_data_bucket
   force_destroy= true
   tags = {
     Name = "raw_data_bucket"
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "raw_data_bucket" {
 }
 
 resource "aws_s3_bucket" "processed_data_bucket" {
-  bucket = var.processed_data_bucket
+  bucket_prefix =var.processed_data_bucket
   force_destroy= true
   tags = {
     Name = "processed_data_bucket"
