32d50ccbd110dceae596af7023bc63557419fb98
Zakii <zakiii1995@outlook.com>
2025-11-24 22:44:07 +0000

    Add load module to orchestrate uploading processed data to S3
    - Coordinates upload using environment-configured bucket and file path
    - Includes logging and error handling for process visibility and robustness

---
+++ src/load/load.py
@@ -0,0 +1,17 @@
+from ..utils.upload_parquet_to_s3_bucket import upload_parquet_data_to_s3_bucket
+import logging
+import os
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+def load_data():
+    logging.info('Uploading processed data to the cloud')
+    try:
+        data= upload_parquet_data_to_s3_bucket(bucket_name=os.getenv('OUTPUT_BUCKET'), file_path=os.getenv('PROCESSED_FILE')) 
+        return data
+    except Exception as e:
+        logging.error(f'Error during upload: {e}')
+        raise
+load_data()
+
---
+++ src/utils/upload_parquet_to_s3_bucket.py
@@ -0,0 +1,32 @@
+import boto3
+import logging 
+from botocore.exceptions import ClientError
+from datetime import datetime
+
+def upload_parquet_data_to_s3_bucket(bucket_name:str, file_path:str):
+    """
+    Saves a local file to an S3 bucket.
+
+    Args:
+        bucket_name (str): The name of the S3 bucket.
+        file_path (str): Local file path to upload.
+
+    Returns:
+        None
+
+    Raises:
+        ClientError: If an error occurs during upload.
+    """
+    s3_client = boto3.client("s3", region_name='eu-west-2')
+    try:
+       
+        logging.info(f"Starting the process of uploading data into the following {bucket_name}")
+        now = datetime.now().strftime("%Y%m%d_%H%M%S")
+        s3_client.upload_file(
+            Bucket=bucket_name, 
+            Filename=file_path,
+            Key=f"{file_path}_{now}")
+        logging.info(f"You have successsfully uploaded data to the {bucket_name} bucket")
+    except ClientError as error:
+        logging.error(f"The following error has occured {error}")
+        raise ClientError(f"The following {error.response} has taken place")
